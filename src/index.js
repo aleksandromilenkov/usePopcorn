2dfed469e9cf0d82483efc1fa7a3baaabe2295ae
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-04-06 19:57:31 +0200

    deleteing unneccessary line

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./Utils/StarRating";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
